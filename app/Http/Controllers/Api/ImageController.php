b7ab12b378e31faafdcd145e540e1942584efd9a
dhruvinprajapati <35141691+dhruvinprajapati@users.noreply.github.com>
2023-03-01 11:34:44 +0530

    image upload only from gallery

--- app/Http/Controllers/Api/ImageController.php
+++ app/Http/Controllers/Api/ImageController.php
@@ -12,9 +12,14 @@ class ImageController extends Controller
     public function imageStore(ImageStoreRequest $request)
     {
         $id = 3;
-        $validatedData = $request->validated();
-        $validatedData['image'] = $request->file('image')->store('PatientsData/'.$id.'/image');
-
+        $validator = $request->validated();
+        // if ($validator->fails()) {
+        //     return response()->json(['response' => 'error', 'message'=> $validator->errors(),'validation_error'=> $validator->errors()],400);
+        // }
+        // $unique_name= time() . '.' . $request->profile_picture->extension();;
+        //         Image::make($request->profile_picture)->save(('images/profile_pictures/').$unique_name);
+        $validator['image'] = $request->file('image')->store('PatientsData/'.$id.'/image');
         return response(Response::HTTP_CREATED);
+
     }
 }
